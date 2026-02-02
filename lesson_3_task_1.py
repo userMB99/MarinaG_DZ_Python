ebcc995bdd0de7c8271787a3e7ca8d08c2d6fac9
userMB99 <tester2509.2024@gmail.com>
2026-02-03 02:46:05 +0300

    добавлены 8 файлов

---
+++ lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Anna", "Ivanova")
+
+print(my_user.sayName())
+print(my_user.sayFam())
+print(my_user.sayName_Fam())
---
+++ smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone:
+	def __init__(self, brand_phone, model_phone, number_phone):
+		self.brand_phone = brand_phone
+		self.model_phone = model_phone
+		self.number_phone = number_phone
+			
+		
